b5b0d0a9ab229f91d4b82134f7f2fea3f1f6d0c0
boris <1024sparrow@gmail.com>
2018-01-04 15:16:08 +0300

    button

--- traliva_kit.js
+++ traliva_kit.js
@@ -9,17 +9,23 @@ else{
 /*
 Виджет Button.
 Принимаемые опцмм:
-    title - если задано, этот текст будет отображаться
-    titleVarName - имя свойства, в котором записан текст кнопки (если изменится значение такого свойства у объекта состояния, кнопка изменит свой текст)
-    togglable - фиксировать ли кнопку. Если кнопка фиксированная, она будет хранить состояние "нажатости", и клик будет означать изменение "нажатости". Если кнопка не фиксированная, клик по ней будет приводить к тому, что в свойство valueVarName будет записано true
-    valueVarName - имя свойства(boolean), значение которого будет меняться при нажатии на кнопку
-Цвета кнопки могут быть заданы только через стили CSS.
+    title - если задано, этот текст будет отображаться, а свойство titleVarName будет проигнорировано. По умолч. - не задано (опирается на объект состояния)
+    titleVarName - имя свойства, в котором записан текст кнопки (если изменится значение такого свойства у объекта состояния, кнопка изменит свой текст). По умолч. 'title'
+    valueVarName - имя свойства(boolean), значение которого будет меняться при нажатии на кнопку. По умолч. 'active'
 */
-function Button(p_wContainer, p_options){// options: title, color('#f00'), valueVarName - имя свойства, в которое сохранять значение
+function Button(p_wContainer, p_options){
     Traliva.WidgetStateSubscriber.call(this, p_wContainer);
     console.log('hello from Button');
-    this.opt = p_options;
-    this.title = p_options.title;
+    if (p_options.hasOwnProperty('title')){
+        this.titleVarName = undefined;
+        this.title = p_options.title;
+    }
+    else{
+        this.titleVarName = p_options.titleVarName || 'title';
+        this.title = '';
+    }
+    this.activeVarName = p_options.activeVarName || 'active';
+    this.active = false;
     var e = Traliva.createElement('<div class="traliva_kit__bn" traliva="e">' + this.title + '</div>', this);
     this.e.addEventListener('click', function(self){return function(){
         self._onClicked();
@@ -29,8 +35,25 @@ function Button(p_wContainer, p_options){// options: title, color('#f00'), value
 Button.prototype = Object.create(Traliva.WidgetStateSubscriber.prototype);
 Button.prototype.constructor = Button;
 Button.prototype.processStateChanges = function(s){
+    if (!s){
+        console.log('epic fail');
+        return;
+    }
+    if (this.titleVarName !== undefined){
+        if (s[this.titleVarName] !== this.title){
+            this.title = s[this.titleVarName] || '';
+            this.e.innerHTML = this.title;
+        }
+    }
+    if (s[this.activeVarName] !== this.active){
+        this.active = s[this.activeVarName];
+        this.e.className = this.active ? 'traliva_kit__bn active' : 'traliva_kit__bn';
+    }
 }
 Button.prototype._onClicked = function(){
+    this.active = !this.active;
+    this._state.active = this.active;
+    this.processStateChanges();
 }
 p_namespace.Button = Button;
 
