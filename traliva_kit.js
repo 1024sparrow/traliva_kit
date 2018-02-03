292a6b24e9e376eb9816d423095498b412cc7469
1024sparrow <1024sparrow@gmail.com>
2018-02-03 14:52:16 +0300

    123

--- traliva_kit.js
+++ traliva_kit.js
@@ -137,7 +137,6 @@ registerHelp('Button', {
         });
 function Button(p_wContainer, p_options){
     Traliva.WidgetStateSubscriber.call(this, p_wContainer, p_options);
-    console.log('hello from Button. Options: '+JSON.stringify(p_options));
     if (p_options.hasOwnProperty('icon')){
         this.icon = true;
     }
@@ -153,7 +152,7 @@ function Button(p_wContainer, p_options){
     }
     this.activeVarName = (p_options.hasOwnProperty('activeVarName')) ? p_options.activeVarName : 'active';
     this.active = false;
-    var e = Traliva.createElement('<div class="traliva_kit__bn" traliva="e"></div>', this);
+    var e = Traliva.createElement('<div traliva="e"></div>', this);
     if (this.icon){
         this.e.style.border = 'none';
         Traliva.background(this.e, p_options.icon);
@@ -164,22 +163,29 @@ function Button(p_wContainer, p_options){
             };})(this.e);
         }
     }
-    else if (p_options.hasOwnProperty('color')){
-        this.e.style.color = p_options.color;
-        this.e.style.border = '1px solid '+p_options.color;
-        if (p_options.hasOwnProperty('border')){
-            this.e.style.border = '1px solid ' + p_options.border.color || p_options.color;
-            if (p_options.border && p_options.border.hasOwnProperty('radius')){
-                this.e.style.borderRadius = p_options.border.radius;
+    else{
+        if (this.title)
+            this.e.innerHTML = this.title;
+        this.e.className = 'traliva_kit__bn';
+        if (p_options.hasOwnProperty('color')){
+            this.e.style.color = p_options.color;
+            this.e.style.border = '1px solid '+p_options.color;
+            if (p_options.hasOwnProperty('border')){
+                this.e.style.border = '1px solid ' + p_options.border.color || p_options.color;
+                if (p_options.border && p_options.border.hasOwnProperty('radius')){
+                    this.e.style.borderRadius = p_options.border.radius;
+                }
             }
         }
     }
-    if (p_options.hasOwnProperty('hover_icon')){
-        if (p_options.hasOwnProperty('icon')){
-            this.e.addEventListener('mouseover', (function(c){return function(){this.style.background = c;};})('url("' + p_options.icon + '")'))
-            this.e.addEventListener('mouseleave', (function(c){return function(){this.style.background = c;};})('url("' + p_options.hover_icon + '")'))
+    if (this.icon){
+        if (p_options.hasOwnProperty('hover_icon')){
+            this.e.addEventListener('mouseover', (function(o,e,f){return function(){f(e,o);};})(p_options.hover_icon, this.e, Traliva.background));
+            this.e.addEventListener('mouseleave', (function(o,e,f){return function(){f(e,o);};})(p_options.icon, this.e, Traliva.background));
         }
-        else{
+    }
+    else{
+        if (p_options.hasOwnProperty('hover_color')){
             this.e.addEventListener('mouseover', (function(c){return function(){this.style.background = c;};})(p_options.hover_color))
             this.e.addEventListener('mouseleave', (function(c){return function(){this.style.background = 'rgba(0,0,0,0)';};})())
         }
@@ -198,14 +204,17 @@ Button.prototype.processStateChanges = function(s){
         if (s[this.titleVarName] !== this.title){
             this.title = s[this.titleVarName] || '';
             if (this.icon)
-                this.e.tooltiptext = this.title;
+                this.e.title = this.title;
             else
                 this.e.innerHTML = this.title;
         }
     }
     if (s[this.activeVarName] !== this.active){
         this.active = s[this.activeVarName];
-        this.e.className = this.active ? 'traliva_kit__bn active' : 'traliva_kit__bn';
+        if (this.icon)
+            ;//
+        else
+            this.e.className = this.active ? 'traliva_kit__bn active' : 'traliva_kit__bn';
     }
 }
 Button.prototype._onClicked = function(){
