c78fab60b46386e4aacf8ed06287019156eb675d
1024sparrow <1024sparrow@gmail.com>
2018-02-04 23:07:26 +0300

    123

--- src/manage.sh
+++ src/manage.sh
@@ -14,22 +14,54 @@ help(){
 
 add(){
     #echo add
-    echo -n "Введите название компонента (как будете вызывать): "
+    echo -n "Введите название компонента (имя класса): "
     read name
-    echo -n "Введите название файла компонента (как будет сохранён код реализации внутри исходного кода TralivaKit): "
-    read file_name
-    echo "-- $name --- $file_name"
-    //
+    file_name=`echo $name | sed -e 's/\([A-Z]\)/_\L\1/g' -e 's/^_//'` # snake_case to camel_case.
+
+    echo >> js/template/links
+    echo "{%% $file_name.js %%}" >> js/template/links
+    echo "p_namespace.$name = $name;" >> js/template/links
+
+    node -e "var a = JSON.parse(fs.readFileSync('js/__meta__', 'utf8'));a.files[0].source.list.push('$file_name.js');fs.writeFileSync('js/__meta__', JSON.stringify(a, undefined, 4));"
+    node -e "var a = JSON.parse(fs.readFileSync('css/__meta__', 'utf8'));a.files[0].source.list.push('$file_name.css');fs.writeFileSync('js/__meta__', JSON.stringify(a, undefined, 4));"
+    touch css/$file_name.css
+
+    echo "registerHelp('$name', {" > js/$file_name.js
+    echo "    title: 'краткого описания нет'," >> js/$file_name.js
+    echo "    //descr: ''," >> js/$file_name.js
+    echo "    //options:{}," >> js/$file_name.js
+    echo "    //stateObj:{}" >> js/$file_name.js
+    echo "});" >> js/$file_name.js
+    echo "function $name(p_wContainer, p_options){" >> js/$file_name.js
+    echo "    Traliva.WidgetStateSubscriber.call(this, p_wContainer, p_options);" >> js/$file_name.js
+    echo "    // ..." >> js/$file_name.js
+    echo "}" >> js/$file_name.js
+    echo "$name.prototype = Object.create(Traliva.WidgetStateSubscriber.prototype);" >> js/$file_name.js
+    echo "$name.prototype.constructor = $name;" >> js/$file_name.js
+    echo "$name.prototype.processStateChanges = function(s){" >> js/$file_name.js
+    echo "    if (!s)" >> js/$file_name.js
+    echo "        console.error('epic fail');" >> js/$file_name.js
+    echo "    // ..." >> js/$file_name.js
+    echo "}" >> js/$file_name.js
 }
 
 list(){
-    list=`cat js/template | grep "{%%" | sed -s 's/{%% //g' | sed -s 's/.js %%}'//g`
-    for i in $list;do echo $i;done
-    #echo $list
+    list=`cat js/template/links | grep "{%%" | sed -s 's/{%% //g' | sed -s 's/.js %%}'//g`
+    for i in $list;do echo $i | sed -e 's/_\([a-z]\)/\U\1/g' -e 's/^\([a-z]\)/\U\1/g';done # camel_case to snake_case used.
 }
 
 remove(){
     echo not implemented
+    PS3="Выберите компонент, который хотите удалить:"
+    list=cat js/template/links | grep "{%%" | sed -s 's/{%% //g' | sed -s 's/.js %%}'//g | sed -e 's/_\([a-z]\)/\U\1/g' -e 's/^\([a-z]\)/\U\1/g'
+    echo ${list[@]}
+    select opt in $list "я передумал - ничего удалять не надо"; do
+        case "$REPLY" in
+            #$(( ${#list[@]}+1 )) ) echo "Goodbye!"; break;;
+            $(( ${#list[@]}+1 )) ) echo "Goodbye!"; break;;
+            *) echo "Некорректная опция. Попробуйте ещё раз." ;;
+        esac
+    done
 }
 
 rename(){
