e7de137b788c13982c976d5e7fd9a97e8a93f228
Борис Васильев <1024sparrow@gmail.com>
2018-03-06 09:19:53 +0300

    ComboBox released

--- src/js/combo_box.js
+++ src/js/combo_box.js
@@ -2,36 +2,65 @@ registerHelp('ComboBox', {
     title: 'выпадающий список',
     //descr: '',
     options:{
-        variants: 'список вариантов. Если задано, то это фиксированный список вариантов, и из объекта состояния набор вариантов мониториться не будет'
+        variants: 'список вариантов (исходный).'
     },
     stateObj:{
         variants: 'список вариантов. Каждый должен быть представлен объектом с полями id и title',
-        current: 'id текущего элемента'
+        variants_changed: '(boolean) флаг о том, что список вариантов изменился, и список отображаемых вариантов необходимо обновить',
+        shared: '(boolean) если true, то компонент не будет сам снимать флаг о том, что данные изменились (внешнее снятие флага)',
+        current: 'id текущего элемента. Особое значение - \'-1\' - означает, что элемент не выбран.'
     }
 });
 function ComboBox(p_wContainer, p_options){
-    var i, t, tt;
     Traliva.WidgetStateSubscriber.call(this, p_wContainer, p_options);
-    p_wContainer.setContent(Traliva.createElement('<select traliva="e"></select>', this));
+    this._options = p_options;
+    this._wContainer = p_wContainer;
     if (p_options.hasOwnProperty('variants')){
-        for (i = 0 ; i < p_options.variants.length ; i++){
-            t = document.createElement('option');
-            t.value = p_options.variants[i].id;
-            tt = document.createTextNode(p_options.variants[i].title);
-            t.appendChild(tt);
-            this.e.appendChild(t);
-        }
+        this._setupContainer(p_options.variants, -1);
     }
-    p_wContainer._onResized = (function(e){return function(w, h){
-        e.style.width = w + 'px';
-        e.style.height = h + 'px';
-    };})(this.e);
-    this.e.value = '-1';
+    else{
+        // "нет вариантов" - отображаем пустой список (сейчас это просто отсутвие виджета)
+    }
 }
 ComboBox.prototype = Object.create(Traliva.WidgetStateSubscriber.prototype);
 ComboBox.prototype.constructor = ComboBox;
 ComboBox.prototype.processStateChanges = function(s){
     if (!s)
         console.error('epic fail');
-    // ...
+    if (s.variants_changed){
+        this._setupContainer(s.variants, (s.current === undefined) ? -1 : s.current);
+        if (!this._options.shared){
+            s.variants_changed = false;
+            this._registerStateChanges();
+        }
+    }
+    else{
+        if (this._current !== s.current){
+            this.e.value = s.current;
+            this._current = s.current;
+        }
+    }
+}
+ComboBox.prototype._setupContainer = function(p_variants, p_current){
+    //элемент select не поддерживает изменение вариантов, поэтому мы полностью заменяем select
+    var i, t, tt;
+    this._wContainer.setContent(Traliva.createElement('<select traliva="e"></select>', this));
+    for (i = 0 ; i < this._options.variants.length ; i++){
+        t = document.createElement('option');
+        t.value = this._options.variants[i].id;
+        tt = document.createTextNode(this._options.variants[i].title);
+        t.appendChild(tt);
+        this.e.appendChild(t);
+    }
+    this.e.value = p_current;
+    this.e.addEventListener('change', (function(self){return function(){
+        self._state.current = self.e.value;
+        self._current = self.e.value;
+        self._registerStateChanges();
+    };})(this))
+    this._current = p_current;
+    this._wContainer._onResized = (function(e){return function(w, h){
+        e.style.width = w + 'px';
+        e.style.height = h + 'px';
+    };})(this.e);
 }
