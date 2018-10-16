14b3d62e8776c5dd4a69967c231186f87b8119de
VasilyevBP <1024sparrow@gmail.com>
2018-10-16 20:32:53 +0300

    123

--- src/js/file_select.js
+++ src/js/file_select.js
@@ -1,45 +1,47 @@
-registerHelp('FileSelect', {
+##USAGE_BEGIN#traliva_kit_debug#
+registerHelp('$FileSelect', {
             title:'Виджет Поле выбора файла из файловой системы пользователя',
-            // Traliva.api.get_filepath(p_file) - должна быть, возвращает путь к файлу.
+            // $Traliva.api.get_filepath(p_file) - должна быть, возвращает путь к файлу.
             options:{
-                valueVarName:'имя свойства объекта состояния, где хранится значение выбранного файла',
-                filter:'по каким расширениям фильтровать. Пример: ".mp3, .mpeg, .wav, .ogg"',
-                color:'цвет',
-                hover_color:'цвет при наведении мышью'
+                $valueVarName:'имя свойства объекта состояния, где хранится значение выбранного файла',
+                $filter:'по каким расширениям фильтровать. Пример: ".mp3, .mpeg, .wav, .ogg"',
+                $color:'цвет',
+                $hover_color:'цвет при наведении мышью'
             }
         });
-function FileSelect(p_wContainer, p_options){
-    Traliva.WidgetStateSubscriber.call(this, p_wContainer, p_options);
-    p_wContainer.setContent(Traliva.createElement('<input type="file" traliva="e" class="traliva_kit__fileselect"></input>', this));
-//wAddBn.setContent(Traliva.createElement('<input type="file" accept=".mp3, .mpeg, .wav, .ogg" traliva="bn_add" class="bn stage2_bn_add"></input>'));
-    this.valueVarName = p_options.valueVarName;
-    this.e.addEventListener('change', (function(self){return function(){
-        var files = self.e.files;
-        files = files.length ? files[0] : undefined;
-        var tmppath = window.URL.createObjectURL(files);
-        self._state[self.valueVarName] = tmppath;
-        self._registerStateChanges();
+##USAGE_END#traliva_kit_debug#
+function $FileSelect($p_wContainer, $p_options){
+    $Traliva.$WidgetStateSubscriber.call(this, $p_wContainer, $p_options);
+    $p_wContainer.$setContent($Traliva.$createElement('<input type="file" traliva="e" class="$traliva_kit__fileselect"></input>', this));
+//wAddBn.$setContent($Traliva.$createElement('<input type="file" accept=".mp3, .mpeg, .wav, .ogg" traliva="bn_add" class="bn stage2_bn_add"></input>'));
+    this.$valueVarName = $p_options.$valueVarName;
+    this.e.addEventListener('change', (function($self){return function(){
+        var $1 = $self.e.files;
+        $1 = $1.length ? $1[0] : undefined;
+        var $2 = window.URL.createObjectURL($1);
+        $self.$_state[$self.$valueVarName] = $2;
+        $self.$_registerStateChanges();
     };})(this));
     
-    if (p_options.hasOwnProperty('filter')){
-        this.e.accept = p_options.filter;
+    if ($p_options.hasOwnProperty('$filter')){
+        this.e.accept = $p_options.$filter;
     }
-    if (p_options.hasOwnProperty('color')){
-        this.e.style.color = p_options.color;
-        this.e.style.border = '1px solid ' + p_options.color;
+    if ($p_options.hasOwnProperty('$color')){
+        this.e.style.color = $p_options.$color;
+        this.e.style.border = '1px solid ' + $p_options.$color;
     }
-    if (p_options.hasOwnProperty('hover_color')){
-        this.e.addEventListener('mouseover', (function(c){return function(){this.style.background = c;};})(p_options.hover_color))
-        this.e.addEventListener('mouseleave', (function(c){return function(){this.style.background = 'rgba(0,0,0,0)';};})())
+    if ($p_options.hasOwnProperty('$hover_color')){
+        this.e.addEventListener('mouseover', (function($1){return function(){this.style.background = $1;};})($p_options.$hover_color))
+        this.e.addEventListener('mouseleave', (function($1){return function(){this.style.background = 'rgba(0,0,0,0)';};})())
     }
-    /*p_wContainer._onResized = (function(self){
+    /*$p_wContainer._onResized = (function($self){
         return function(w,h){
-            self.e.style.width = (w - 18) + 'px';
+            $self.e.style.width = (w - 18) + 'px';
         }
     })(this);*/
 }
-FileSelect.prototype = Object.create(Traliva.WidgetStateSubscriber.prototype);
-FileSelect.prototype.constructor = FileSelect;
-FileSelect.prototype.processStateChanges = function(s){
+$FileSelect.prototype = Object.create($Traliva.$WidgetStateSubscriber.prototype);
+$FileSelect.prototype.constructor = $FileSelect;
+$FileSelect.prototype.processStateChanges = function(s){
     //boris here: применить изменения в выбранном файле
 }
--- src/js/line_edit.js
+++ src/js/line_edit.js
@@ -1,60 +1,64 @@
-registerHelp('LineEdit', {
+##USAGE_BEGIN#traliva_kit_debug#
+registerHelp('$LineEdit', {
             title: 'Виджет строка ввода',
             options:{
-                placeholder:'строка подсказки вроде "введите ..(что-то)"',
-                requireVarName: 'имя свойства(boolean), true которого означает, что нужно записать в объект состояния значение этого текстового поля. Если не задано, в объекте состояния значения будет обновляться при каждом изменении текста',
+                $placeholder:'строка подсказки вроде "введите ..(что-то)"',
+                $requireVarName: 'имя свойства(boolean), true которого означает, что нужно записать в объект состояния значение этого текстового поля. Если не задано, в объекте состояния значения будет обновляться при каждом изменении текста',
                 textVarName:'текст в поле редактирования. Это значение как для задания предустановленного значения, так и для считывания другими компонентами введённого пользователем текста',
-                color:'цвет текста и рамочки',
-                hover_color:'цвет фона при наведении мышью'
+                $color:'цвет текста и рамочки',
+                $hover_color:'цвет фона при наведении мышью'
             }
         });
-function LineEdit(p_wContainer, p_options){
-    Traliva.WidgetStateSubscriber.call(this, p_wContainer, p_options);
-    this.requireVarName;
+##USAGE_END#traliva_kit_debug#
+function $LineEdit($p_wContainer, $p_options){
+    $Traliva.$WidgetStateSubscriber.call(this, $p_wContainer, $p_options);
+    this.$requireVarName;
 
-    p_wContainer.setContent(Traliva.createElement('<input type="text" traliva="e" class="traliva_kit__lineedit"></input>', this));
-    p_wContainer._onResized = (function(self){
+    $p_wContainer.$setContent($Traliva.createElement('<input type="text" traliva="e" class="$traliva_kit__lineedit"></input>', this));
+    $p_wContainer.$_onResized = (function($self){
         return function(w,h){
-            self.e.style.width = (w - 32) + 'px';
+            $$self.e.style.width = (w - 32) + 'px';
         }
     })(this);
-    if (p_options.hasOwnProperty('placeholder'))
-        this.e.placeholder = p_options.placeholder;
-    if (p_options.hasOwnProperty('color')){
-        this.e.style.color = p_options.color;
-        this.e.style.border = '1px solid ' + p_options.color;
+    if ($p_options.hasOwnProperty('$placeholder'))
+        this.e.placeholder = $p_options.$placeholder;
+    if ($p_options.hasOwnProperty('$color')){
+        this.e.style.color = $p_options.$color;
+        this.e.style.border = '1px solid ' + $p_options.$color;
     }
-    if (p_options.hasOwnProperty('hover_color')){
-        this.e.addEventListener('mouseover', (function(c){return function(){this.style.background = c;};})(p_options.hover_color))
-        this.e.addEventListener('mouseleave', (function(c){return function(){this.style.background = 'rgba(0,0,0,0)';};})())
+    if ($p_options.hasOwnProperty('$hover_color')){
+        this.e.addEventListener('mouseover', (function($1){return function(){this.style.background = $1;};})($p_options.$hover_color))
+        this.e.addEventListener('mouseleave', (function($1){return function(){this.style.background = 'rgba(0,0,0,0)';};})())
     }
-    if (p_options.hasOwnProperty('textVarName'))
-        this.textVarName = p_options.textVarName;
+    if ($p_options.hasOwnProperty('$textVarName'))
+        this.$textVarName = $p_options.$textVarName;
+##USAGE_BEGIN#traliva_kit_debug#
     else
-        console.error('LineEdit: textVarName - обязательное поле для задания в options');
-    if (p_options.hasOwnProperty('requireVarName')){
-        this.requireVarName = p_options.requireVarName;
+        console.error('$LineEdit: $textVarName - обязательное поле для задания в options');
+##USAGE_END#traliva_kit_debug#
+    if ($p_options.hasOwnProperty('$requireVarName')){
+        this.$requireVarName = $p_options.$requireVarName;
     }
     else{
-        this.e.addEventListener('input', (function(self){return function(){
-            self._state[self.textVarName] = self.e.value;
-            self._registerStateChanges();
+        this.e.addEventListener('input', (function($self){return function(){
+            $self.$_state[$self.$textVarName] = $self.e.value;
+            $self.$_registerStateChanges();
         }})(this));
         // event 'change' fires only on focus off
     }
 }
-LineEdit.prototype = Object.create(Traliva.WidgetStateSubscriber.prototype);
-LineEdit.prototype.constructor = LineEdit;
-LineEdit.prototype.processStateChanges = function(s){
-    if (this.requireVarName){
+$LineEdit.prototype = Object.create($Traliva.$WidgetStateSubscriber.prototype);
+$LineEdit.prototype.constructor = $LineEdit;
+$LineEdit.prototype.$processStateChanges = function(s){
+    if (this.$requireVarName){
         // опция выдачи строго по запросу не протестирована.
         // надеюсь, при запуске ошибок не возникнет
-        if (s[this.requireVarName]){
-            s[this.requireVarName] = false;
-            s[this.textVarName] = this.e.value;
-            this._registerStateChanges();
+        if (s[this.$requireVarName]){
+            s[this.$requireVarName] = false;
+            s[this.$textVarName] = this.e.value;
+            this.$_registerStateChanges();
         }
     }
-    if (this.e.value !== s[this.textVarName])
-        this.e.value = s[this.textVarName];
+    if (this.e.value !== s[this.$textVarName])
+        this.e.value = s[this.$textVarName];
 }
