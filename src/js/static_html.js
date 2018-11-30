43dbc8b3c6eaa55d9f16bf99f78a910c9b6dd4d5
1024sparrow <1024sparrow@gmail.com>
2018-11-30 22:03:03 +0300

    123

--- src/js/static_html.js
+++ src/js/static_html.js
@@ -3,15 +3,17 @@ registerHelp('$StaticHtml', {
     title: 'Тупо отображатель статического HTML',
     //descr: '',
     options:{
-        html: 'HTML, отображаемый данным виджетом.',
-        htmlVarName: 'имя переменной в объекте состояния, в которой хранится HTML-код, который данный виджет должен отображать. Если не задано, то отображаемый код будет неизменным. Если значение переменной имеет неопределённое значение, то отображается HTML-код, заданный в опции "html".'
+        $html: 'HTML, отображаемый данным виджетом.',
+        $htmlVarName: 'имя переменной в объекте состояния, в которой хранится HTML-код, который данный виджет должен отображать. Если не задано, то отображаемый код будет неизменным. Если значение переменной имеет неопределённое значение, то отображается HTML-код, заданный в опции "html".'
     }
     //stateObj:{}
 });
 #USAGE_END#traliva_kit_debug##
 function $StaticHtml($p_wContainer, $p_options){
     $Traliva.$WidgetStateSubscriber.call(this, $p_wContainer, $p_options);
-    // ...
+    this.$_options = $p_options;
+    if ($p_options.$html)
+        $p_wContainer.$setContent($Traliva.$createElement($p_options.$html));
 }
 $StaticHtml.prototype = Object.create($Traliva.$WidgetStateSubscriber.prototype);
 $StaticHtml.prototype.constructor = $StaticHtml;
@@ -20,5 +22,5 @@ $StaticHtml.prototype.$processStateChanges = function(s){
         console.error('epic fail');
         return;
     }
-    // ...
+    //if ()
 }
