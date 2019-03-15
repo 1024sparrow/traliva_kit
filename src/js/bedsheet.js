9dec18d910622d6d7488283d3ab23a2501b4e45d
1024sparrow <1024sparrow@gmail.com>
2019-03-15 04:25:36 +0300

    Bedsheet: сделан параметризуемым. Убраны все зашитые опции.

--- src/js/bedsheet.js
+++ src/js/bedsheet.js
@@ -2,12 +2,17 @@
 registerHelp('$Bedsheet', {
     title: 'Простыня - это контейнер, который отображает контент на широких (десктопных) экранах не на всю ширину, а лишь на узкой вертикальной полосе по центру',
     descr: 'Используйте конструктор как функцию, чтобы сгенерировать в лейауте описание виджета, обёрнутого этим контейнером',
-    //options:{},
+    options:{
+        padding: 'В пикселях. По умолчанию 10.',
+        border: '{color: <цвет>, width: <ширина в пикселях>, background: <фон подложки под контентом>}. Если задано, то будет ограничение контента по ширине. Если задано, то ширина линии границы по умолчанию - 1 пиксел.'
+        //borderColor: '',
+        //borderWidth: ''
+    },
     //stateObj:{}
 });
 #USAGE_END#traliva_kit_debug##
 function $Bedsheet($p_wContainer, $p_options, $p_widgets){
-    var $children, $content, $1, $2 = {};
+    var $children, $content, $1, $2 = {}, $3;
     if (this.constructor !== $Bedsheet){ // это не конструктор, а тупо функция
         // сокращённая семантика для сокращения лейаутов.
         // Должны вернуть фрагмент описателя лейаута.
@@ -32,21 +37,32 @@ function $Bedsheet($p_wContainer, $p_options, $p_widgets){
     $1 = $Traliva.$createElement('<div traliva="$1"></div>', $2);
     $2.$1.style.margin = 'auto';
     //$2.$1.style.background = 'rgba(255,255,255,0.3)';
-    $2.$1.style.background = '#ffc';
-    $2.$1.style.padding = '10px';
-    $2.$1.style.borderRight = '1px solid #420';
-    $2.$1.style.borderLeft = '1px solid #420';
+    $2.$1.style.padding = '' + ($p_options.$padding || 10) + 'px';
+    if ($3 = $p_options.$border){
+        $2.$1.style.background = $3.$background || '#888';
+        $3 = ($3.$width || 1) + 'px solid ' + ($3.$color || 'black');
+        $2.$1.style.borderRight = $3;
+        $2.$1.style.borderLeft = $3;
+    }
     $1.style.overflow = '';
-    $p_wContainer.$_onResized = (function($0, $1){return function($w, $h){
+    $p_wContainer.$_onResized = (function($0, $1, $p_options){return function($w, $h){
+        var $2, $3;
         this.$_content.style.height = '' + $h + 'px';
-        $0.style.minHeight = '' + ($h - 20) + 'px';
-        var $2 = $w * 0.9;
-        if ($2 > 800)
-            $2 = 800;
-        $0.style.width = '' + $2 + 'px';
+        $3 = $p_options.$padding || 10;
+        $0.style.minHeight = '' + ($h - 2*$3) + 'px';
+        if ($p_options.$border){
+            $2 = $w * 0.9;
+            if ($2 > 800)
+                $2 = 800;
+            $0.style.width = '' + $2 + 'px';
+        }
+        else{
+            $2 = $w;
+            $0.style.width = '100%';
+        }
         console.log('content: ', $1);//
         $1.$_widget.$resize($2, 0);
-    };})($2.$1, $content);
+    };})($2.$1, $content, $p_options);
     $2.$1.appendChild($content.$_widget.$_div);
     $p_wContainer.$setContent($1);
     $p_wContainer.$_content.style.overflow = 'auto';
