76a3b7056d1552b034ad7ce5dacd23594a3fbea4
1024sparrow <1024sparrow@gmail.com>
2019-04-15 08:04:40 +0300

    123

--- src/js/90412_map_view.js
+++ src/js/90412_map_view.js
@@ -1,12 +1,15 @@
+//State JSON: {"ss__data":[{"ss__lat":57.873608,"ss__lon":39.535165}]}
 #USAGE_BEGIN#traliva_kit_debug##
 registerHelp('$90412MapView', {
     title: 'Виджет карты (yandex maps)',
     //descr: '',
     options:{
-        dataVarName: 'имя переменной, в которой хранятся данные'
+        dataVarName: 'имя переменной, в которой хранятся данные по объектам',
+        mapVarName: 'имя переменной, в которой хранятся настройки самой карты'
     },
     stateObj:{
-        data: 'список объектов.\n[{\n  pos:{\n    lat: широта в градусах,\n    lon: долгота в градусах\n  },\n  ...\n}]'
+        data: 'список объектов.\n[{\n  pos:{\n    lat: широта в градусах,\n    lon: долгота в градусах\n  },\n  ...\n}]',
+        map: 'объект {  \n}'
     }
 });
 #USAGE_END#traliva_kit_debug##
@@ -17,11 +20,11 @@ function $90412MapView($p_wContainer, $p_options, $p_widgets){
     //this.$lat;
     //this.$lon;
 
-    var $content, $1 = {};
+    var $content, $1 = this;
     $content = $Traliva.$createElement(`
         <!--<script src="http://api-maps.yandex.ru/2.0/?load=package.full&lang=ru-RU"></script>-->
         <div traliva="$eMap" style="width: 100%; height: 100%; background: #888;">
-    `, $1);
+    `, this);
 
     $p_wContainer.$_onResized = function($w, $h){
         $1.$eMap.style.width = '' + $w + 'px';
@@ -29,24 +32,10 @@ function $90412MapView($p_wContainer, $p_options, $p_widgets){
     };
 
     ymaps.ready((function($1){return function(){
-        var myMap = new ymaps.Map($1, {center: [57.873608,39.535165],zoom: 16, type: "yandex#map", behaviors: ["default", "scrollZoom"]});
+        $1.$myMap = new ymaps.Map($1.$eMap, {center: [57.873608,39.535165],zoom: 16, type: "yandex#map", behaviors: ["default", "scrollZoom"]});
         // Создаем метку и задаем изображение для ее иконки
-        var myPlacemark = new ymaps.Placemark(
-            [57.873608,39.535165],
-            {
-                balloonContent: ""
-            },
-            {   
-                iconImageHref: "/data/template/images/pointer.png", // картинка иконки
-                iconImageSize: [128, 73], // размеры картинки
-                iconImageOffset: [ -51,-63] // смещение картинки
-            }
-        );
-        myMap.controls.add("zoomControl").add("mapTools").add(new ymaps.control.TypeSelector(["yandex#map", "yandex#satellite", "yandex#hybrid", "yandex#publicMap"]));
-
-        // Добавление метки на карту
-        myMap.geoObjects.add(myPlacemark);
-    };})($1.$eMap));
+        $1.$myMap.controls.add("zoomControl").add("mapTools").add(new ymaps.control.TypeSelector(["yandex#map", "yandex#satellite", "yandex#hybrid", "yandex#publicMap"]));
+    };})(this));
     $p_wContainer.$setContent($content);
 };
 $90412MapView.prototype = Object.create($Traliva.$WidgetStateSubscriber.prototype);
@@ -58,14 +47,33 @@ $90412MapView.prototype.$processStateChanges = function(s){
         return;
     }
     #USAGE_END#debug##
-    var $1;
+    var $1, $2;
     if (s)
         $1 = s[this.$dataVarName];
     if ($1){
         if ($1.length === 1){
             $1 = $1[0];
             if ($1.$lat !== this.$lat || $1.$lon !== this.$lon){
+                console.log('update...');
                 // update
+                // ...
+                //if (!this.$myMap)
+                //    return;
+                //this.$myMap.geoObjects.removeAll();
+                $2 = new ymaps.Placemark(
+                    [$1.$lat, $1.$lon],
+                    {
+                        balloonContent: ''
+                    },
+                    {
+                        //iconImageHref: "/data/template/images/pointer.png", // картинка иконки
+                        //iconImageHref: "/static/pointer.gif", // картинка иконки
+                        iconImageHref: "file:///home/maryanna/da/tmp/pointer.gif", // картинка иконки
+                        iconImageSize: [128, 73], // размеры картинки
+                        iconImageOffset: [ -51,-63] // смещение картинки
+                    }
+                );
+                this.$myMap.geoObjects.add($2);
                 // ...
                 this.$lat = $1.$lat;
                 this.$lon = $1.$lon;
