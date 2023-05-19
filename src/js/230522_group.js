f8d0917ec843997df9370328b755755d1462855f
Борис Васильев <boris@traliva.ru>
2023-05-19 15:36:02 +0300

    230522Group

--- src/js/230522_group.js
+++ src/js/230522_group.js
@@ -26,18 +26,15 @@ function $230522Group($p_wContainer, $p_options, $p_widgets){
 	;
 	$children = $Traliva.$WidgetStateSubscriber.call(this, $p_wContainer, $p_options, $p_widgets);
 	this.$e = $Traliva.$createElement('<div traliva="$content" class="$TralivaKit__230522Group__content">'+$p_options.$title+'</div>', this, '$TralivaKit__230522Group');
-	/*this.$e.style.border = 'solid red 10px';
-	this.$e.style.borderRadius = '10px';*/
 	$1 = $children.$content[0].$_widget.$_div.style;
-	//$1.border = 'solid #400 1px';
-	//$1.borderRadius = '10px';
 	$1.position = 'relative';
 	this.$e.appendChild($children.$content[0].$_widget.$_div);
 	$children.$content[0].$_widget.$_div.style.top = '4px';
-	$p_wContainer.$_onResized = (function($1, $2){ return function($w,$h){
+	$p_wContainer.$_onResized = (function($1, $2, $3){ return function($w,$h){
 		$1.left = '20px';
-		$2.$resize($w - 40, $h/2);
-	};})($1, $children.$content[0].$_widget);
+		$3.style.minHeight = $3.style.maxHeight = $3.style.height = '' + ($h - 2) + 'px';
+		$2.$resize($w - 40, $h - 35);
+	};})($1, $children.$content[0].$_widget, this.$e);
 	$p_wContainer.$setContent(this.$e);
 };
 $230522Group.prototype = Object.create($Traliva.$WidgetStateSubscriber.prototype);
