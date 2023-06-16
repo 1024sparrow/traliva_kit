84d3f1f7ed7b3ce9f7fd3f2c93705a46d917557e
Boris Vasilyev <1024sparrow@gmail.com>
2023-06-16 06:40:43 +0300

    develop-v2.2.230614MultiHorBarChart

--- src/js/230614_multi_hor_bar_chart.js
+++ src/js/230614_multi_hor_bar_chart.js
@@ -10,7 +10,40 @@ registerHelp('$230614MultiHorBarChart', {
 #USAGE_END#traliva_kit_debug##
 function $230614MultiHorBarChart($p_wContainer, $p_options, $p_widgets){
 	$Traliva.$WidgetStateSubscriber.call(this, $p_wContainer, $p_options, $p_widgets);
-	// ...
+	var $1, $2;
+	this.$constItemHeight = 24;
+	this.$_tt = document.createElement('table');
+	this.$containers = [];
+	this.$w = 0;
+	this.$h = 0;
+	this.$scrollPos = 0;
+	this.$options = $p_options;
+
+	$p_wContainer.$_onResized = (function($self){
+		return function($w, $h){
+			$self.$w = $w;
+			$self.$h = $h;
+			$self.$_update();
+		}
+	})(this);
+	this.$_tt.addEventListener(
+		'wheel',
+		(function($self){
+			return function($event) {
+				var
+					$1 = $self.$scrollPos + $event.deltaY/5,
+					$2 = $self.$_state.$list.length * $self.$constItemHeight,
+					$3 = $2 - $self.$h
+				;
+				if ($1 < $2){
+					$self.$scrollPos = ($1 < $3) ? $1 : $3;
+				}
+				$self.$_update();
+			}
+		})(this)
+	);
+	this.$_tt.className = '$traliva_kit__230614MultiHorBarChart';
+	$p_wContainer.$setContent(this.$_tt);
 };
 $230614MultiHorBarChart.prototype = Object.create($Traliva.$WidgetStateSubscriber.prototype);
 $230614MultiHorBarChart.prototype.constructor = $230614MultiHorBarChart;
@@ -20,5 +53,51 @@ $230614MultiHorBarChart.prototype.$processStateChanges = function(s){
 		return;
 	}
 	// ...
+};
+$230614MultiHorBarChart.prototype.$_update = function(){
+	var $1, $2, $3, $4, $5;
+
+	if (this.$scrollPos > this.$_state.$list.length * this.$constItemHeight){
+		this.$scrollPos = this.$_state.$list.length * this.$constItemHeight - this.$h;
+	}
+	if (this.$scrollPos < 0){
+		this.$scrollPos = 0;
+	}
+
+	$1 = this.$h / this.$constItemHeight + 1;
+	for ($2 = this.$containers.length ; $2 < $1 ; ++$2){
+		$3 = this.$_tt.insertRow();
+		$4 = $3.insertCell();
+		$4.innerHTML = '1111';//
+		$5 = $3.insertCell();
+		$5.innerHTML = '2222';//
+		this.$containers.push({$eRow: $3, $eTitle: $4, $eDiagram: $5});
+	}
+
+
+
+	/*for ($2 = 0 ; $2 < this.$containers.length ; ++$2){
+		this.$containers[$2].$root.style.width = '' + this.$w + 'px';
+	}
+
+	for (
+		$1 = 0, $2 = parseInt(this.$scrollPos / this.$constItemHeight);
+		$1 < this.$containers.length;
+		++$1, ++$2
+	){
+		this.$containers[$1].index = $2;
+		this.$containers[$1].$1.className =
+			this.$_state.$selected.lastIndexOf($2) >= 0 ?
+				($2 < this.$_state.$list.length ? '$1 $on' : '$1') :
+				($2 < this.$_state.$list.length ? '$1 $off' : '$1')
+		;
+		this.$containers[$1].$root.className = ($2 < this.$_state.$list.length ? '$item $used' : '$item');
+		this.$containers[$1].$2.innerHTML =
+			$2 < this.$_state.$list.length ?
+				this.$_state.$list[$2] :
+				''
+		;
+	}
+	this.$_tt.style.marginTop = '-' + this.$scrollPos%24 + 'px';*/
 };
 //$230614MultiHorBarChart.$widgetsFields = [];
