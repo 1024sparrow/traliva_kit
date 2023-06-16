adf8d1a84f29102100fa5d23c8e0a0e34cfa7f5b
Борис Васильев <boris.vasilyev@tmhsmart.ru>
2023-06-16 21:39:05 +0300

    develop-v2.2.230614MultiHorBarChart

--- src/js/230614_multi_hor_bar_chart.js
+++ src/js/230614_multi_hor_bar_chart.js
@@ -11,7 +11,7 @@ registerHelp('$230614MultiHorBarChart', {
 function $230614MultiHorBarChart($p_wContainer, $p_options, $p_widgets){
 	$Traliva.$WidgetStateSubscriber.call(this, $p_wContainer, $p_options, $p_widgets);
 	var $1, $2;
-	this.$constItemHeight = 24;
+	this.$constItemHeight = 64;
 	this.$_tt = document.createElement('table');
 	this.$_tt.style.margin = '0px';
 	this.$containers = [];
@@ -32,7 +32,7 @@ function $230614MultiHorBarChart($p_wContainer, $p_options, $p_widgets){
 		(function($self){
 			return function($event) {
 				var
-					$1 = $self.$scrollPos + $event.deltaY/5,
+					$1 = $self.$scrollPos + $event.deltaY/10,
 					$2 = $self.$_state.$list.length * $self.$constItemHeight,
 					$3 = $2 - $self.$h
 				;
@@ -70,49 +70,77 @@ $230614MultiHorBarChart.prototype.$_update = function(){
 	$1 = this.$h / this.$constItemHeight + 1;
 	for ($2 = this.$containers.length ; $2 < $1 ; ++$2){
 		$3 = this.$_tt.insertRow();
-		//$3.height = '64px';
 		$4 = $3.insertCell();
-		$4.innerHTML = '1111';//
+		$4.style.textAlign = 'right';
+		$4.style.padding = '0 10px';
 		$5 = $3.insertCell();
 		$6 = document.createElement('canvas');
-		$6.height = 64;
+		$6.height = this.$constItemHeight;
+		$6.style.width = '100%';
 		$4.style.borderRight = '1px solid #808284';
 		$5.appendChild($6);
-		this.$containers.push({$eRow: $3, $eTitle: $4, $eDiagram: $5});
+		this.$containers.push({$eRow: $3, $eTitle: $4, $eDiagram: $6, $eDiagramTd: $5});
 	}
 
 
 
 	for ($2 = 0 ; $2 < this.$containers.length ; ++$2){
-		//this.$containers[$2].$root.style.width = '' + this.$w + 'px';
+		this.$containers[$2].$eDiagram.style.width = '0px';
 	}
-
-
-
-	/*for ($2 = 0 ; $2 < this.$containers.length ; ++$2){
-		this.$containers[$2].$root.style.width = '' + this.$w + 'px';
-	}
-
+	$4 = 0; // ширина первой колонки в виде числа
 	for (
 		$1 = 0, $2 = parseInt(this.$scrollPos / this.$constItemHeight);
 		$1 < this.$containers.length;
 		++$1, ++$2
 	){
-		this.$containers[$1].index = $2;
-		this.$containers[$1].$1.className =
-			this.$_state.$selected.lastIndexOf($2) >= 0 ?
-				($2 < this.$_state.$list.length ? '$1 $on' : '$1') :
-				($2 < this.$_state.$list.length ? '$1 $off' : '$1')
-		;
-		this.$containers[$1].$root.className = ($2 < this.$_state.$list.length ? '$item $used' : '$item');
-		this.$containers[$1].$2.innerHTML =
+		$3 = this.$containers[$1];
+		$3.$eTitle.innerHTML =
 			$2 < this.$_state.$list.length ?
-				this.$_state.$list[$2] :
+				this.$_state.$list[$2].$title :
 				''
 		;
+		$5 = $3.$eTitle.offsetWidth;
+		if ($5 > $4){
+			$4 = $5;
+		}
 	}
-	this.$_tt.style.marginTop = '-' + this.$scrollPos%24 + 'px';*/
+	//console.log('230616: ', $4);//
+	//$4 = '' + (this.$w - $4) + 'px'; // ширина второй колоки в виде строки
+	$4 = this.$w - $4; // ширина второй колоки в виде числа
+	for ($1 = 0 ; $1 < this.$containers.length ; ++$1){
+		$3 = this.$containers[$1];
+		$3.$eDiagram.style.width = '' + $4 + 'px';
+		$3.$eDiagram.width = $4;
+		$3.$eDiagram.height = this.$constItemHeight - 4; // 4 - магическое число. Я так и не нашёл способа избавиться от этого отступа в 4 пиксела.
+		//$3.$eDiagramTd.style.height = '' + this.$constItemHeight + 'px';
+	}
+	for (
+		$1 = 0, $2 = parseInt(this.$scrollPos / this.$constItemHeight);
+		$1 < this.$containers.length;
+		++$1, ++$2
+	){
+		$3 = this.$containers[$1];
+		this.$_updateListItem(
+			$3.$eDiagram,
+			$2 < this.$_state.$list.length ?
+				this.$_state.$list[$2] :
+				undefined
+		);
+	}
+	this.$_tt.style.marginTop = '-' + this.$scrollPos%(this.$constItemHeight) + 'px';
 };
-$230614MultiHorBarChart.prototype.$_updatelistItem = function($a_element, $a_descriptor){
+$230614MultiHorBarChart.prototype.$_updateListItem = function($a_element, $a_descriptor){
+	var
+		$context = $a_element.getContext('2d'),
+		$w = $a_element.width,
+		$h = $a_element.height
+	;
+	$context.fillStyle = '#afa';
+	$context.fillRect(0,5,$w - 5,$h - 10);
+	//$context.crearRect(0, 0, this.$w, this);
+	if ($a_descriptor){
+	}
+	else{
+	}
 };
 //$230614MultiHorBarChart.$widgetsFields = [];
